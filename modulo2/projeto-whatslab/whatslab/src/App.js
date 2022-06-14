4f2ae2daca6d167243369c99e3af9adb4834624a
thailaesteves <104744254+thailaesteves@users.noreply.github.com>
2022-06-13 21:18:28 -0300

    arrumando projeto com ajuda da sugestao de projetos prontos

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,69 +1,82 @@
-import React from "react";
-import './App.css';
-import ListaDeMensagem from "./components/ListaDeMensagem";
+import React from 'react';
+import styled from 'styled-components'
 
+const AppContainer = styled.div`
+  border: 1px solid green;
+  height: 100vh;
+  box-sizing: border-box;
+  width: 600px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`
+const MensagensTela = styled.div`
+  border: 1px solid red;
+  background-color: lightblue;
+  flex-grow: 1;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+  `
 
-class App extends React.Component() {
+const Inputs = styled.div`
+  border: 1px solid blue;
+  display: flex;
+  `
+
+const NameInput = styled.input` 
+width: 100px;
+`
+
+const MsgInput = styled.input` 
+ flex-grow: 1;
+`
+
+class App extends React.Component {
   state = {
-    mensagem: [
-    {
-        nome: "Ana",
-        msg: "OIII"
-    },
-    {
-        nome: "Nilde",
-        msg: "OLA"
+    lista: [
+     
+    ],
+    valorInputUser: '',
+    valorInputText: ''
+  }
+
+  onChangeUserValue = (event) => {
+    this.setState({valorInputUser: event.target.value})
+  }
+
+  onChangeUserMsg = (event) => {
+    this.setState({valorInputText: event.target.value})
+  }
+
+  enviarMsg = () => {
+    const novaMsg = {
+      user: this.state.valorInputUser,
+      text: this.state.valorInputText
     }
-]
+    const newArray = [novaMsg, ...this.state.lista]
+    this.setState({lista: newArray, valorInputText: '', valorInputUser: ''})
+  }
 
-};
-
-adicionaPessoa = () => {
-    const novaMensagem = {
-        nome: this.state.valorInputNome,
-        nome: this.state.valorInputMensagem
-    }
-
-    const novoMensagens = [...this.setState, novaMensagem]
-
-    this.setState({mensagem: novoMensagens})
-}
-
-onChangeInputNome = (event) => {
-    this.setState({ valorInputNome: event.target.value})
-}
-
-onChangeInputMensagem = (event) => {
-    this.setState({ valorInputMensagem: event.target.value})
-}
-
-render () {
-    const lista = this.state.mensagem.map((mensagem)=> {
+  render() {
+    
         return (
-            <p>
-                {mensagem.nome}: {mensagem.mensagem} 
-            </p>
-        )
-    })
-
-    return (
-        <div>
-            <input 
-            value={this.state.valorInputNome}
-            onChange={this.onChangeInputNome}
-            placeholder={"Nome"}
-            />
-             <input 
-            value={this.state.valorInputMensagem}
-            onChange={this.onChangeInputMensagem}
-            placeholder={"Mensagem"}
-            />
-            <div>
-                <div>{lista}</div>
-            </div>
-        </div>
-    )
+            <AppContainer>
+            <MensagensTela>
+              {this.state.lista.map((msg, index) => {
+                return <p key={index}><b>{msg.user}</b>: {msg.text}</p>
+              }
+                )}
+        
+            </MensagensTela>
+            <Inputs>
+            <NameInput onChange={this.onChangeUserValue} value={this.state.valorInputUser} placeholder={'Nome'}/>
+            <MsgInput onChange={this.onChangeUserMsg} value={this.state.valorInputText} placeholder={'Mensagem'}/>
+            <button onClick={this.enviarMsg}>Enviar</button>
+            </Inputs>
+            </AppContainer>
+        );
+  }
 }
-}
-
-export default App
+ 
+export default App;
